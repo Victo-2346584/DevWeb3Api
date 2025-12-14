86ab957bcedbcae913dfc6eab232a5c72e7f9d93
Hugo Nadeau <2346584@carrefour.cegep.com>
2025-12-14 15:57:58 -0500

    test6

--- config.ts
+++ config.ts
@@ -1,5 +1,4 @@
 /* eslint-disable n/no-process-env */
-
 import path from 'path';
 import dotenv from 'dotenv';
 import moduleAlias from 'module-alias';
@@ -7,13 +6,14 @@ import moduleAlias from 'module-alias';
 // Check the env
 const NODE_ENV = process.env.NODE_ENV ?? 'development';
 
-// Configure "dotenv"
-const result2 = dotenv.config({
-  path: path.join(__dirname, `./config/.env.${NODE_ENV}`),
-});
-if (result2.error) {
-  throw result2.error;
-}
+// Mettre en commentaire
+// // Configure "dotenv"
+// const result2 = dotenv.config({
+//   path: path.join(__dirname, `./config/.env.${NODE_ENV}`),
+// });
+// if (result2.error) {
+//   throw result2.error;
+// }
 
 // Configure moduleAlias
 if (__filename.endsWith('js')) {
